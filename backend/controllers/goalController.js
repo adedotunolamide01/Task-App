fae99201254eb66e17b5a3c77f0342d3d45ee36d
adedotunolamide01 <adedotuntoyese1@gmail.com>
2023-01-17 23:46:02 +0100

    task app

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -31,7 +31,7 @@ const updateGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Goal not found');
   }
-  const user = await User.findById(req.user.id);
+
   if (!req.user) {
     res.status(401);
     throw new Error('User not found');
